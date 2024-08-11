6ac1dfff829bb1c3a1f082c115815c6ff9a28743
Sri Sujan <mbsrisujan@gmail.com>
2024-08-11 14:21:46 +0530

    update name

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import {
   NavigationMenu,
@@ -7,6 +8,26 @@ import {
 } from "@/components/ui/navigation-menu"; // Adjust the import path based on your project structure
 
 const Navbar: React.FC = () => {
+  const [storedUserName, setStoredUserName] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Check for the username in session storage initially
+    const username = sessionStorage.getItem("username");
+    setStoredUserName(username);
+
+    // Listen for changes in sessionStorage
+    const handleStorageChange = () => {
+      const updatedUsername = sessionStorage.getItem("username");
+      setStoredUserName(updatedUsername);
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
+
   return (
     <div className="p-2 flex flex-row justify-between items-center shadow-md">
       <Link to="/" className="">
@@ -14,14 +35,6 @@ const Navbar: React.FC = () => {
       </Link>
       <NavigationMenu>
         <NavigationMenuList>
-          {/* <NavigationMenuItem>
-            <Link
-              to="/"
-              className={`${navigationMenuTriggerStyle()} text-[16px] font-medium`}
-            >
-              Home
-            </Link>
-          </NavigationMenuItem> */}
           <NavigationMenuItem>
             <Link
               to="/playground"
@@ -46,23 +59,11 @@ const Navbar: React.FC = () => {
               Battleground
             </Link>
           </NavigationMenuItem>
-          {/* <NavigationMenuItem>
-            <DropdownMenu>
-              <NavigationMenuTrigger
-                className={`${navigationMenuTriggerStyle()} text-[16px] font-medium`}
-              >
-                Account
-              </NavigationMenuTrigger>
-              <DropdownMenuContent>
-                <Link to="/profile" className="flex items-center p-2">
-                  <User className="mr-2 h-4 w-4" />
-                  Profile
-                </Link>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </NavigationMenuItem> */}
         </NavigationMenuList>
       </NavigationMenu>
+      {storedUserName && (
+        <p className="text-lg font-medium m-2">{storedUserName}</p>
+      )}
     </div>
   );
 };

95bd989e0f70c3021c3bf04df26189845c4e7892
Siddesh Devarakonda <siddeshdslr@gmail.com>
2024-08-16 16:33:44 +0530

    set scrollable element

--- frontend/src/components/problem/InputOutput.tsx
+++ frontend/src/components/problem/InputOutput.tsx
@@ -214,132 +214,142 @@ const InputOutput: React.FC<InputOutputProps> = ({
         </div>
       </div>
       <ScrollArea className="h-full">
-        {subResponse && subResponse.is_solved ? (
-          <div className="flex items-start justify-between p-4">
-            <div className="flex flex-col">
-              <p className="text-lg font-semibold text-green-500">
-                All test cases passed!
-              </p>
-              <p className="text-sm text-[#ffffff99]">
-                Total test cases passed: {subResponse.total_passed}
-              </p>
-            </div>
-            <div className="flex flex-col mr-10">
-              <div className="flex flex-row text-sm text-[#ffffff99] gap-1">
-                <p className="text-white">Execution Time:</p>
-                <p>{subResponse ? `${subResponse.elapsed_time} sec` : "N/A"}</p>
+        <div className="h-[35rem]">
+          {subResponse && subResponse.is_solved ? (
+            <div className="flex items-start justify-between p-4">
+              <div className="flex flex-col">
+                <p className="text-lg font-semibold text-green-500">
+                  All test cases passed!
+                </p>
+                <p className="text-sm text-[#ffffff99]">
+                  Total test cases passed: {subResponse.total_passed}
+                </p>
               </div>
-              <div className="flex flex-row text-sm text-[#ffffff99] gap-1">
-                <p className="text-white">Memory Usage:</p>
-                <p>{subResponse ? `${subResponse.memory_used} MB` : "N/A"}</p>
-              </div>
-            </div>
-          </div>
-        ) : subResponse && !subResponse.is_solved ? (
-          <div className="flex items-start justify-between p-4">
-            <div className="flex flex-col">
-              <p className="text-lg font-semibold text-red-500">
-                Some test cases failed!
-              </p>
-              <p className="text-sm text-[#ffffff99]">
-                Total test cases passed: {subResponse.total_passed}
-              </p>
-            </div>
-            <div className="flex flex-col mr-10">
-              <div className="flex flex-row text-sm text-[#ffffff99] gap-1">
-                <p className="text-white">Execution Time:</p>
-                <p>{subResponse ? `${subResponse.elapsed_time} sec` : "N/A"}</p>
-              </div>
-              <div className="flex flex-row text-sm text-[#ffffff99] gap-1">
-                <p className="text-white">Memory Usage:</p>
-                <p>{subResponse ? `${subResponse.memory_used} MB` : "N/A"}</p>
-              </div>
-            </div>
-          </div>
-        ) : null}
-
-        <div className="flex mt-4 px-4">
-          {problem.examples
-            .filter((example) => example.id !== "custom")
-            .map((example, index) => (
-              <div className="mr-2 mt-2 cursor-pointer" key={example.id}>
-                <div className="flex items-center gap-y-4">
-                  <div
-                    className={`text-sm font-medium inline-flex items-center transition-all duration-300 rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap ${
-                      activeTestCaseId === index
-                        ? "bg-[#27272a] text-white"
-                        : "bg-[#2d2d2d] text-gray-500"
-                    }`}
-                    onClick={() => setActiveTestCaseId(index)}
-                  >
-                    {subResponse ? (
-                      subResponse.results[index]?.test_passed ? (
-                        <span className="w-1 h-1 mr-2 bg-green-500 rounded-full inline-block"></span>
-                      ) : (
-                        <span className="w-1 h-1 mr-2 bg-red-500 rounded-full inline-block"></span>
-                      )
-                    ) : null}
-                    Case {index + 1}
-                  </div>
+              <div className="flex flex-col mr-10">
+                <div className="flex flex-row text-sm text-[#ffffff99] gap-1">
+                  <p className="text-white">Execution Time:</p>
+                  <p>
+                    {subResponse ? `${subResponse.elapsed_time} sec` : "N/A"}
+                  </p>
+                </div>
+                <div className="flex flex-row text-sm text-[#ffffff99] gap-1">
+                  <p className="text-white">Memory Usage:</p>
+                  <p>{subResponse ? `${subResponse.memory_used} MB` : "N/A"}</p>
                 </div>
               </div>
-            ))}
+            </div>
+          ) : subResponse && !subResponse.is_solved ? (
+            <div className="flex items-start justify-between p-4">
+              <div className="flex flex-col">
+                <p className="text-lg font-semibold text-red-500">
+                  Some test cases failed!
+                </p>
+                <p className="text-sm text-[#ffffff99]">
+                  Total test cases passed: {subResponse.total_passed}
+                </p>
+              </div>
+              <div className="flex flex-col mr-10">
+                <div className="flex flex-row text-sm text-[#ffffff99] gap-1">
+                  <p className="text-white">Execution Time:</p>
+                  <p>
+                    {subResponse ? `${subResponse.elapsed_time} sec` : "N/A"}
+                  </p>
+                </div>
+                <div className="flex flex-row text-sm text-[#ffffff99] gap-1">
+                  <p className="text-white">Memory Usage:</p>
+                  <p>{subResponse ? `${subResponse.memory_used} MB` : "N/A"}</p>
+                </div>
+              </div>
+            </div>
+          ) : null}
 
-          <div className="mr-2 mt-2 cursor-pointer">
-            <div className="flex items-center gap-y-4">
-              <div
-                className={`text-sm font-medium inline-flex items-center transition-all duration-300 rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap ${
-                  activeTestCaseId === problem.examples.length - 1
-                    ? "bg-[#27272a] text-white"
-                    : "bg-[#2d2d2d] text-gray-500"
-                }`}
-                onClick={() => setActiveTestCaseId(problem.examples.length - 1)}
-              >
-                Custom Input
+          <div className="flex mt-4 px-4">
+            {problem.examples
+              .filter((example) => example.id !== "custom")
+              .map((example, index) => (
+                <div className="mr-2 mt-2 cursor-pointer" key={example.id}>
+                  <div className="flex items-center gap-y-4">
+                    <div
+                      className={`text-sm font-medium inline-flex items-center transition-all duration-300 rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap ${
+                        activeTestCaseId === index
+                          ? "bg-[#27272a] text-white"
+                          : "bg-[#2d2d2d] text-gray-500"
+                      }`}
+                      onClick={() => setActiveTestCaseId(index)}
+                    >
+                      {subResponse ? (
+                        subResponse.results[index]?.test_passed ? (
+                          <span className="w-1 h-1 mr-2 bg-green-500 rounded-full inline-block"></span>
+                        ) : (
+                          <span className="w-1 h-1 mr-2 bg-red-500 rounded-full inline-block"></span>
+                        )
+                      ) : null}
+                      Case {index + 1}
+                    </div>
+                  </div>
+                </div>
+              ))}
+
+            <div className="mr-2 mt-2 cursor-pointer">
+              <div className="flex items-center gap-y-4">
+                <div
+                  className={`text-sm font-medium inline-flex items-center transition-all duration-300 rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap ${
+                    activeTestCaseId === problem.examples.length - 1
+                      ? "bg-[#27272a] text-white"
+                      : "bg-[#2d2d2d] text-gray-500"
+                  }`}
+                  onClick={() =>
+                    setActiveTestCaseId(problem.examples.length - 1)
+                  }
+                >
+                  Custom Input
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        {problem.examples.map((example, index) =>
-          activeTestCaseId === index ? (
-            <div key={example.id} className="font-semibold my-4 px-4">
-              <p className="text-sm font-medium mt-4 text-gray-500">Input:</p>
-              <textarea
-                className="w-full rounded-lg text-sm font-light border px-3 py-2 bg-[#27272a] border-transparent text-white mt-2"
-                value={example.inputText}
-                onChange={(e) => handleInputChange(example.id, e.target.value)}
-                disabled={example.id !== "custom"}
-              />
-              {example.id !== "custom" && (
-                <>
-                  <p className="text-sm font-medium mt-4 text-gray-500">
-                    Expected:
-                  </p>
-                  <textarea
-                    className="w-full rounded-lg text-sm font-light border px-3 py-2 bg-[#27272a] border-transparent text-white mt-2"
-                    value={example.outputText}
-                    onChange={(e) =>
-                      handleOutputChange(example.id, e.target.value)
-                    }
-                    disabled
-                  />
-                </>
-              )}
-              <p className="text-sm font-medium mt-4 text-gray-500">
-                Run Output:
-              </p>
-              <textarea
-                className="w-full h-fit rounded-lg text-sm font-light border px-3 py-2 bg-[#27272a] border-transparent text-white mt-2"
-                value={example.runText || ""}
-                onChange={(e) =>
-                  handleRunTextChange(example.id, e.target.value)
-                }
-                readOnly
-              />
-            </div>
-          ) : null
-        )}
+          {problem.examples.map((example, index) =>
+            activeTestCaseId === index ? (
+              <div key={example.id} className="font-semibold my-4 px-4">
+                <p className="text-sm font-medium mt-4 text-gray-500">Input:</p>
+                <textarea
+                  className="w-full rounded-lg text-sm font-light border px-3 py-2 bg-[#27272a] border-transparent text-white mt-2"
+                  value={example.inputText}
+                  onChange={(e) =>
+                    handleInputChange(example.id, e.target.value)
+                  }
+                  disabled={example.id !== "custom"}
+                />
+                {example.id !== "custom" && (
+                  <>
+                    <p className="text-sm font-medium mt-4 text-gray-500">
+                      Expected:
+                    </p>
+                    <textarea
+                      className="w-full rounded-lg text-sm font-light border px-3 py-2 bg-[#27272a] border-transparent text-white mt-2"
+                      value={example.outputText}
+                      onChange={(e) =>
+                        handleOutputChange(example.id, e.target.value)
+                      }
+                      disabled
+                    />
+                  </>
+                )}
+                <p className="text-sm font-medium mt-4 text-gray-500">
+                  Run Output:
+                </p>
+                <textarea
+                  className="w-full h-fit rounded-lg text-sm font-light border px-3 py-2 bg-[#27272a] border-transparent text-white mt-2"
+                  value={example.runText || ""}
+                  onChange={(e) =>
+                    handleRunTextChange(example.id, e.target.value)
+                  }
+                  readOnly
+                />
+              </div>
+            ) : null
+          )}
+        </div>
       </ScrollArea>
     </div>
   );
